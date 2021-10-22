ad4f0a0d6a1082adc2195a515ba42a23184108de
JotaroE21 <jusseaumejonathan5@gmail.com>
2021-10-22 23:01:39 +0200

    Niveau carapuce

--- carapuce.c
+++ carapuce.c
@@ -1,30 +1,33 @@
-/*
+/**
  * Fichier crée par JUSSEAUME Jonathan le 21/10/2021
  */
 
 
-/*
+/**
  * Les librairies
  */
 #include<stdio.h>
-
-/*
- * Les constantes
- */
-
-/*
+#include<stdlib.h>
+#include<string.h>
+/**
  * Les valeurs booléenes
  */
 #define TRUE 1
 #define FALSE 0
-/*
+/**
+ * Les valeurs possibles de blind
+ */
+#define BLINDED 1
+#define NOT_BLINDED 0
+/**
  * Les valeurs que peuvent prendre une case
  */
 #define CHECKED 2
 #define BLACK 1
 #define WHITE 0
 #define UNKNOWN -1
-/*
+
+/**
  * Les valeurs d'orientation de la tortue
  */
 #define LEFT 0
@@ -32,7 +35,7 @@
 #define RIGHT 2
 #define DOWN 3
 
-/*
+/**
  * Les modes pour demander les cases 
  */
 #define MOST_LEFT 0
@@ -40,16 +43,12 @@
 #define MOST_UP 1
 #define MOST_DOWN 0
 
-/*
+/**
  * Le nombre de lignes et de colonnes
  */
 #define NUMBER_LINES 12
 #define NUMBER_COLUMNS 12
 
-
-/*
- * Les structures
- */
 
 /**
  * Structure qui définit ce qui constitue une position sur la grille
@@ -62,17 +61,6 @@ typedef struct Position {
 } Position;
 
 /**
- * Structure qui définit ce qui constitue un joueur du jeu
- * Il se définit par un numéro, un nombre de points et un booléen
- * permettant de savoir si le joueur est retiré du jeu ou non
- */
-typedef struct Player {
-    int number;
-    int score;
-    int isRemove;
-} Player;
-
-/**
  * Structure qui définit ce qui constitue la tortue de notre joueur.
  * Elle est définit par sa position sur le plateau, sa direction
  * ainsi que le fait que son crayon soit baissé ou non
@@ -83,31 +71,300 @@ typedef struct Turtle {
     int penIsDown;
 } Turtle;
 
+
+/**
+ * Structure qui définit ce qui constitue un joueur du jeu
+ * Il se définit par un numéro, un nombre de points et un booléen
+ * permettant de savoir si le joueur est retiré du jeu ou non
+ */
+typedef struct Player {
+    Turtle turtle;
+    int number;
+    int score;
+    int isRemove;
+} Player;
+
+
 /**
  * Structure contenant toutes les informations essentielles de la partie.
  * Les informations que je possède sur la grille, par défaut toutes les cases valent UNKNOWN,
  * la liste des joueurs, le numéro de notre joueur, notre tortue
  */
 typedef struct Game {
-    int **grille;
+    int **grid;
     Player *players;
+    int numberPlayers;
     int myNumero;
-    Turtle turtle;
 } Game;
 
-/*
- * Les prototypes de fonction
- */
+
 Game initGame();
 
-/*
- * Le MAIN
+/**
+ * Renvoie à partir d'une ligne envoyé par le jeu, une liste
+ * de tous les entiers séparés par des espaces
+ * @param line la ligne que l'on veut parser
+ * @return une liste d'entiers (exemple: line= 1 2 3, return: [1, 2, 3])
  */
+int *parseLine(char *line);
+
+/**
+ * Affiche la liste passée en paramètre dans la sortie d'erreur
+ * @param list
+ * @param size
+ */
+void displayArray(int *list, int size);
+
+void updateGame(Game *game);
+
+void addSubstringToIntList(int *list,
+                           int *size,
+                           char *line,
+                           int indexStart,
+                           int indexEnd);
+
+int checkIfBlinded(Game game);
+
+int checkIfParalized(Game game);
+
+
 int main(void) {
-    perror("--LANCEMENT DU PROGRAMME: CARAPUCE EST PRÊT--");
+    fprintf(stderr, "STEP\n");
+    Game game = initGame();
+    int nbTours = 0;
+    fprintf(stderr, "STEP\n");
+    updateGame(&game);
+    fprintf(stderr, "STEP\n");
+    fprintf(stdout, "NOBLIND\n");
+    fflush(stdout);
+    fprintf(stderr, "STEP\n");
+    int number;
+    if (checkIfBlinded(game) == FALSE) {
+        fprintf(stderr, "STEP\n");
+        fprintf(stdout, "REVEALL %d %d\n", 6, MOST_LEFT);
+        fflush(stdout);
+        char buffer[2];
+        fgets(buffer, 2, stdin);
+        number = atoi(buffer);
+        if (number == -1) {
+            for (int i = 0; i < NUMBER_COLUMNS; i++) {
+                game.grid[6][i] = WHITE;
+            }
+        } else {
+            for (int i = 0; i < number; i++) {
+                game.grid[6][i] = WHITE;
+            }
+            game.grid[6][number] = BLACK;
+        }
+    }
+    fprintf(stdout, "NOPARALYZE\n");
+    fflush(stdout);
+    if (checkIfParalized(game) == FALSE) {
+        fprintf(stderr, "|%d|\n", game.players[game.numberPlayers].turtle.direction);
+        if (game.players[game.myNumero].turtle.direction == LEFT) {
+            fprintf(stdout, "TELEPORT 6 %d; SWITCHPEN; ROTATE 1; ROTATE 1; PASS\n", number);
+            fflush(stdout);
+        } else if (game.players[game.myNumero].turtle.direction == RIGHT) {
+            fprintf(stdout, "TELEPORT 6 %d; SWITCHPEN; PASS\n", number);
+            fflush(stdout);
+        } else if (game.players[game.myNumero].turtle.direction == UP) {
+            fprintf(stdout, "TELEPORT 6 %d; SWITCHPEN; ROTATE 1; PASS\n", number);
+            fflush(stdout);
+        } else if (game.players[game.myNumero].turtle.direction == DOWN) {
+            fprintf(stdout, "TELEPORT 6 %d; SWITCHPEN; ROTATE 0; PASS\n", number);
+            fflush(stdout);
+        }
+
+    }
+    nbTours++;
+    updateGame(&game);
+    fprintf(stderr, "STEP\n");
+    fprintf(stdout, "NOBLIND\n");
+    fflush(stdout);
+    fprintf(stderr, "STEP\n");
+    fflush(stderr);
+    int number2 = -1;
+    if (checkIfBlinded(game) == FALSE) {
+        fprintf(stdout, "REVEALL %d %d\n", 6, 1);
+        fflush(stdout);
+        char buffer[3];
+        fflush(stdin);
+        fgets(buffer, 3, stdin);
+        fgets(buffer, 3, stdin);
+        fgets(buffer, 3, stdin);
+        number2 = atoi(buffer);
+        fprintf(stderr, "BUFFER %s\n", buffer);
+        if (number2 == -1) {
+            for (int i = 0; i < NUMBER_COLUMNS; i++) {
+                game.grid[6][i] = WHITE;
+            }
+        } else {
+            for (int i = NUMBER_COLUMNS - 1; i > number2; i--) {
+                game.grid[6][i] = WHITE;
+            }
+            game.grid[6][number2] = BLACK;
+        }
+    }
+    fprintf(stdout, "NOPARALYZE\n");
+    fflush(stdout);
+    if (checkIfParalized(game) == FALSE) {
+        fprintf(stdout, "MOVE %d\n", number2 - number);
+        fflush(stdout);
+    }
+    nbTours++;
+
+
+    while (nbTours < 150) {
+        fprintf(stdout, "NOBLIND\n");
+        fflush(stdout);
+        fprintf(stdout, "NOREVEAL\n");
+        fflush(stdout);
+        fprintf(stdout, "NOPARALYZE\n");
+        fflush(stdout);
+        fprintf(stdout, "PASS\n");
+        fflush(stdout);
+
+        nbTours++;
+        for (int i = 0; i < NUMBER_LINES; i++) {
+            displayArray(game.grid[i], NUMBER_COLUMNS);
+        }
+        for (int i = 0; i < game.numberPlayers; i++) {
+            fprintf(stderr, "J%d, x=%d, y=%d, score=%d \n", game.players[i].number, game.players[i].turtle.position.x,
+                    game.players[i].turtle.position.y, game.players[i].score);
+        }
+    }
+
 }
 
-/*
- * Les fonctions
- */
+int *parseLine(char *line) {
+    int sizeResult = 0;
+    int *result = (int *) malloc(sizeResult * sizeof(int));
+    int index = 0;
+    int indexStartNumber = 0;
+    /*
+     * On parcourt la ligne
+     */
+    while (line[index] != '\0') {
+        /*
+         * Si on est sur un espace alors il faut ajouter le nombre
+         * à notre liste de nombre
+         */
+        if (line[index] == ' ') {
+            addSubstringToIntList(result, &sizeResult, line, indexStartNumber, index);
+            indexStartNumber = index + 1;
+        }
+        index++;
+    }
+    addSubstringToIntList(result, &sizeResult, line, indexStartNumber, index);
+    return result;
+}
 
+void addSubstringToIntList(int *list,
+                           int *size,
+                           char *line,
+                           int indexStart,
+                           int indexEnd) {
+    char buffSubstring[indexEnd - indexStart + 1];
+    memcpy(buffSubstring, &line[indexStart], indexEnd - indexStart);
+    buffSubstring[indexEnd - indexStart] = '\0';
+    *size = *size + 1;
+    list = (int *) realloc(list, *size * sizeof(int));
+    list[*size - 1] = atoi(buffSubstring);
+}
+
+void displayArray(int *list, int size) {
+    fprintf(stderr, "[");
+    for (int i = 0; i < size; i++) {
+        fprintf(stderr, "%d", list[i]);
+        if (size - 1 != i) {
+            fprintf(stderr, ",");
+        }
+    }
+    fprintf(stderr, "] \n");
+}
+
+Game initGame() {
+    Game game;
+    char buffer[30];
+    fgets(buffer, 30, stdin);
+    int *array = parseLine(buffer);
+    game.myNumero = array[1];
+    game.numberPlayers = array[0];
+    game.players = (Player *) malloc(game.numberPlayers * sizeof(Player));
+    game.grid = (int **) malloc(NUMBER_LINES * sizeof(int *));
+    for (int i = 0; i < NUMBER_LINES; i++) {
+        game.grid[i] = (int *) malloc(NUMBER_COLUMNS * sizeof(int));
+        for (int j = 0; j < NUMBER_COLUMNS; j++) {
+            game.grid[i][j] = UNKNOWN;
+        }
+    }
+    free(array);
+    return game;
+}
+
+void updateGame(Game *game) {
+    char buffer[15];
+    for (int i = 0; i < game->numberPlayers; i++) {
+        fgets(buffer, 15, stdin);
+        int *playerInfo = parseLine(buffer);
+        game->players[i].number = playerInfo[0];
+        game->players[i].score = playerInfo[1];
+        game->players[i].isRemove = playerInfo[2];
+        free(playerInfo);
+    }
+    fgets(buffer, 15, stdin);
+    int nbCells = atoi(buffer);
+    fprintf(stderr, "cell=%d\n", nbCells);
+    for (int i = 0; i < nbCells; i++) {
+        fgets(buffer, 15, stdin);
+        int *cellInfo = parseLine(buffer);
+        fprintf(stderr, "cell j=%d l=%d, c=%d \n", cellInfo[0], cellInfo[1], cellInfo[2]);
+        game->grid[cellInfo[1]][cellInfo[2]] = CHECKED;
+        displayArray(game->grid[cellInfo[1]], 12);
+    }
+    int i;
+    for (i = 0; i < game->numberPlayers; i++) {
+        fgets(buffer, 15, stdin);
+        int *playerTurtleInfo = parseLine(buffer);
+        game->players[i].turtle.position.x = playerTurtleInfo[1];
+        game->players[i].turtle.position.y = playerTurtleInfo[2];
+        game->players[i].turtle.direction = playerTurtleInfo[3];
+        game->players[i].turtle.penIsDown = playerTurtleInfo[4];
+        free(playerTurtleInfo);
+    }
+}
+
+
+int checkIfBlinded(Game game) {
+    fprintf(stderr, "%d \n", game.numberPlayers);
+    int rvalue = FALSE;
+    for (int i = 0; i < game.numberPlayers; i++) {
+        fprintf(stderr, "%d \n", i);
+        char buffer[15];
+        fgets(buffer, 15, stdin);
+        int *playerBlind = parseLine(buffer);
+        fprintf(stderr, "%d  %d \n", playerBlind[0], playerBlind[1]);
+        if (playerBlind[0] == game.myNumero &&
+            playerBlind[1] == BLINDED) {
+            rvalue=TRUE;
+        }
+    }
+    return rvalue;
+}
+
+int checkIfParalized(Game game) {
+    fprintf(stderr, "%d \n", game.numberPlayers);
+    int rvalue = FALSE;
+    for (int i = 0; i < game.numberPlayers; i++) {
+        fprintf(stderr, "%d \n", i);
+        char buffer[15];
+        fgets(buffer, 15, stdin);
+        int *playerBlind = parseLine(buffer);
+        fprintf(stderr, "%d  %d \n", playerBlind[0], playerBlind[1]);
+        if (playerBlind[0] == game.myNumero &&
+            playerBlind[1] == BLINDED) {
+            rvalue=TRUE;
+        }
+    }
+    return rvalue;
+}
